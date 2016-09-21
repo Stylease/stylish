9ec44bca7b22fac12469bf2f96d9134a6ea733fe
sagarmulchandani <sagarmulchandani.sm@gmail.com>
2016-09-21 19:28:00 +0530

    done

--- js/controllers.js
+++ js/controllers.js
@@ -1095,6 +1095,7 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
             //     }) == -1;
             // }
             return _.findIndex($scope.timestamps, function(key) {
+                console.log("aaaaaaa".key);
                 key.setHours(0, 0, 0, 0);
                 current.setHours(0, 0, 0, 0);
                 // console.log(new Date(key), new Date(current));
