165626fcb38da0828d1c32abfc2ba70e7e81f7a5
Vinod Beloshe <vinodbeloshe12@gmail.com>
2016-10-17 19:21:52 +0530

    password

--- js/controllers.js
+++ js/controllers.js
@@ -2301,6 +2301,7 @@ angular.module('phonecatControllers', ['templateservicemod', "calenderService", 
         $scope.cartCheckout = $.jStorage.get("cartCheckout");
         NavigationService.getOrderById($stateParams.orderid, function (data) {
             $scope.orderDetails = data.data;
+            console.log("aaaaaaa", $scope.orderDetails);
         })
     })
     .controller('SorryCtrl', function ($scope, TemplateService, NavigationService, $timeout) {
